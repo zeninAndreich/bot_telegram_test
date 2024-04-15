fb0c9a95153b5d2ee1f31988be4856b4c3c54aee
zenin.a.s <zenin.a.s@effective-group.ru>
2024-04-15 08:54:26 +0300

    Версия 2. Начинаю создавать бота

--- Otpravnay_tochka.py
+++ Otpravnay_tochka.py
@@ -8,6 +8,16 @@ name = ''
 @myBot.message_handler(commands = ['start'])
 def start_message(message):
     myBot.send_message(message.chat.id,'Привет, мой догорой друг!!!')
+    mark_up = telebot.types.ReplyKeyboardMarkup()
+    btn1 = telebot.types.KeyboardButton('Помогите!!! ')
+    btn2 = telebot.types.KeyboardButton('Все пока!!! ')
+    btn3 = telebot.types.KeyboardButton('Кнопка на запас!!! ')
+    mark_up.row(btn1,btn2)
+    mark_up.row(btn3)
+    myBot.send_message(message.chat.id, 'Мы тут сделали управление:',reply_markup=mark_up)
+    with open('logi_start.txt','a') as f:
+        print(message.from_user.username,file=f)
+
 
 @myBot.message_handler(commands=['help'])
 def help_message(message):
@@ -24,6 +34,21 @@ def reg_name(message):
     global name
     name = message.text
     myBot.send_message(message.chat.id, f'Рад приветствовать тебя {name} !!! :)')
+    my_keyboard = telebot.types.InlineKeyboardMarkup()
+    key_one = telebot.types.InlineKeyboardButton(text = 'Больше 18', callback_data=1)
+    key_two = telebot.types.InlineKeyboardButton(text = 'Меньше 18', callback_data=2)
+    my_keyboard.add(key_one)
+    my_keyboard.add(key_two)
+    myBot.send_message(message.chat.id, 'Сколько тебе лет? ',reply_markup=my_keyboard)
+
+
+@myBot.callback_query_handler(func = lambda call: True)
+def callback_keyboard(call):
+    if call.data == '1':
+        myBot.send_message(call.message.chat.id, 'Тебе уже можно все!!! ')
+    if call.data == '2':
+        myBot.send_message(call.message.chat.id, 'Пока ничего нельзя!!!')
+
 
 #@myBot.message_handler(content_types= ['voice'])
 #def voice_message(message):
